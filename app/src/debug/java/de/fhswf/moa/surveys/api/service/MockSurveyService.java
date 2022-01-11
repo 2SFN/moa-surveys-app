322edbf486da75d06731fd942e5f6cb902e03988
SFN <service.sfn+bitbucket@gmail.com>
2022-01-11 21:34:15 +0100

    + Mock-Implementierung der Survey Service-Schnittstelle für Testing hinzugefügt.

---
+++ app/src/debug/java/de/fhswf/moa/surveys/api/service/MockSurveyService.java
@@ -0,0 +1,158 @@
+package de.fhswf.moa.surveys.api.service;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import de.fhswf.moa.surveys.api.OnFailureListener;
+import de.fhswf.moa.surveys.api.OnSuccessListener;
+import de.fhswf.moa.surveys.api.parser.ParsingException;
+import de.fhswf.moa.surveys.model.InfoQuestion;
+import de.fhswf.moa.surveys.model.InputQuestion;
+import de.fhswf.moa.surveys.model.MultiSelectQuestion;
+import de.fhswf.moa.surveys.model.RatingQuestion;
+import de.fhswf.moa.surveys.model.SingleSelectQuestion;
+import de.fhswf.moa.surveys.model.Survey;
+
+/**
+ * Mock-Service, welcher immer einige statische Test-Daten zurückgibt.
+ * <p>
+ * Alternativ kann konfiguriert werden, dass die Methoden immer eine negative (Fehler) Meldung
+ * als Antwort erzeugen (s. {@link this#setGenerateNegativeResponses(boolean)}).
+ */
+public class MockSurveyService implements SurveyService {
+    public static final int DELAY = 2000;
+
+    private final Handler handler = new Handler(Looper.getMainLooper());
+
+    private boolean generateNegativeResponses;
+
+    private final ArrayList<Survey> surveys;
+
+    public MockSurveyService() {
+        this.surveys = new ArrayList<>();
+
+        // Fill in some example data
+        fillExampleSurveys();
+    }
+
+    @Override
+    public void fetchSurveyList(@Nullable OnSuccessListener<List<Survey>> onSuccessListener,
+                                @Nullable OnFailureListener onFailureListener) {
+        if (generateNegativeResponses) {
+            errorCall("FetchSurveyList", onFailureListener);
+        } else if (onSuccessListener != null) {
+            handler.postDelayed(() -> onSuccessListener.onSuccess(
+                    Collections.unmodifiableList(surveys)), DELAY);
+        }
+    }
+
+    @Override
+    public void fetchSurveyDetails(String id,
+                                   @Nullable OnSuccessListener<Survey> onSuccessListener,
+                                   @Nullable OnFailureListener onFailureListener) {
+        if (generateNegativeResponses) {
+            errorCall("FetchSurveyDetails", onFailureListener);
+        } else if (onSuccessListener != null) {
+            handler.postDelayed(() -> {
+                for (Survey c : surveys) {
+                    if (c.getId().equals(id)) {
+                        onSuccessListener.onSuccess(c);
+                        return;
+                    }
+                }
+
+                if (onFailureListener != null)
+                    onFailureListener.onFailure(new RuntimeException("Survey not found! (mock)"));
+            }, DELAY);
+        }
+    }
+
+    @Override
+    public void submitResponses(@NonNull String id, @NonNull JSONObject body,
+                                @Nullable OnSuccessListener<Void> onSuccessListener,
+                                @Nullable OnFailureListener onFailureListener) {
+        if (generateNegativeResponses) {
+            errorCall("SubmitResponse", onFailureListener);
+        } else if (onSuccessListener != null) {
+            handler.postDelayed(() -> onSuccessListener.onSuccess(null), DELAY);
+        }
+    }
+
+    public boolean isGenerateNegativeResponses() {
+        return generateNegativeResponses;
+    }
+
+    public void setGenerateNegativeResponses(boolean generateNegativeResponses) {
+        this.generateNegativeResponses = generateNegativeResponses;
+    }
+
+    @SuppressWarnings("ThrowableNotThrown")
+    private void errorCall(String source, @Nullable OnFailureListener onFailureListener) {
+        if (onFailureListener != null) {
+            handler.postDelayed(() -> new ParsingException("Mock-Error for %s!", source), DELAY);
+        }
+    }
+
+    private void fillExampleSurveys() {
+        surveys.add(new Survey(
+                        "S1", "Lieblingsfarbe",
+                        "Kurze Fragen zu deinen Gedanken über verschiedene Farben."
+                ).addQuestion(new InfoQuestion(
+                        "Q1-1", "Umfrage zu Lieblingsfarben",
+                        "Im Folgenden findest du einige Fragen zum Thema Farben!"
+                )).addQuestion(new InputQuestion(
+                        "Q1-2", "Gedanken",
+                        "Beschreibe, wie du dich fühlst, wenn du über das Thema " +
+                                "'Farben' nachdenkst.", 500
+                )).addQuestion(new SingleSelectQuestion(
+                        "Q1-3", "Lieblingsfarbe",
+                        "Wähle hier deine absolute Lieblingsfarbe aus. Sollte deine " +
+                                "Farbe nicht aufgeführt sein, wähle bitte eine Farbe, die " +
+                                "deiner am ähnlichsten sieht."
+                ).addOption("Rot").addOption("Blau").addOption("Weiß").addOption("Schwarz")
+                        .addOption("Grün").addOption("Magenta").addOption("Aubergine")
+                        .addOption("Kristall").addOption("Türkis").addOption("Lachs")
+                        .addOption("Salbei").addOption("Fuchsia").addOption("Beige")
+                ).addQuestion(new MultiSelectQuestion(
+                        "Q1-4", "Hässliche Farben",
+                        "Wähle bis zu fünf Farben, die absolut unschön aussehen.",
+                        5
+                ).addOption("Rot").addOption("Blau").addOption("Weiß").addOption("Schwarz")
+                        .addOption("Grün").addOption("Magenta").addOption("Aubergine")
+                        .addOption("Kristall").addOption("Türkis").addOption("Lachs")
+                        .addOption("Salbei").addOption("Fuchsia").addOption("Beige")
+                ).addQuestion(new RatingQuestion(
+                        "Q1-5", "Feedback zur Umfrage",
+                        "Was hältst du von dieser Umfrage?"
+                ))
+        );
+
+        surveys.add(new Survey(
+                "S2", "Beispiel 1",
+                "Diese Umfrage enthält keine tatsächlichen Fragen und ist nur hier, " +
+                        "damit alles ein wenig gefüllter ausschaut."
+        ).addQuestion(new InfoQuestion("Q2-1", "Leer", "Hier ist nichts.")));
+
+        surveys.add(new Survey(
+                "S3", "Beispiel 2",
+                "Diese Umfrage enthält keine tatsächlichen Fragen und ist nur hier, " +
+                        "damit alles ein wenig gefüllter ausschaut."
+        ).addQuestion(new InfoQuestion("Q3-1", "Leer", "Hier ist nichts.")));
+
+        surveys.add(new Survey(
+                "S4", "Beispiel 3",
+                "Diese Umfrage enthält keine tatsächlichen Fragen und ist nur hier, " +
+                        "damit alles ein wenig gefüllter ausschaut."
+        ).addQuestion(new InfoQuestion("Q4-1", "Leer", "Hier ist nichts.")));
+    }
+
+}

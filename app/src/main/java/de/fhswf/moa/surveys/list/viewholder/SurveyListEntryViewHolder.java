386129833abbebde19db7276fed7df08a594fe64
SFN <service.sfn+bitbucket@gmail.com>
2022-02-04 20:47:51 +0100

    ~ Survey List-Items: Dynamische Hintergrundfarbe.

--- app/src/main/java/de/fhswf/moa/surveys/list/viewholder/SurveyListEntryViewHolder.java
+++ app/src/main/java/de/fhswf/moa/surveys/list/viewholder/SurveyListEntryViewHolder.java
@@ -4,28 +4,37 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 
 import de.fhswf.moa.surveys.R;
 import de.fhswf.moa.surveys.list.item.SurveyListItem;
+import de.fhswf.moa.surveys.util.ColorGenerator;
 
 public class SurveyListEntryViewHolder extends BaseViewHolder<SurveyListItem>
         implements View.OnClickListener {
 
     private SurveyListItem currentSurvey;
-    private TextView title;
-    private TextView description;
+
+    private final CardView card;
+    private final TextView title;
+    private final TextView description;
 
     public SurveyListEntryViewHolder(@NonNull View itemView) {
         super(itemView);
+
+        this.card = itemView.findViewById(R.id.cardview);
         this.title = itemView.findViewById(R.id.title);
         this.description = itemView.findViewById(R.id.description);
-        itemView.setOnClickListener(this);
 
+        itemView.setOnClickListener(this);
     }
 
     @Override
     public void bind(SurveyListItem item) {
         this.currentSurvey = item;
+
+        // Card Hintergrund
+        card.setCardBackgroundColor(ColorGenerator.MATERIAL.getColor(item.getSurvey()));
 
         title.setText(item.getSurvey().getTitle());
         description.setText(item.getSurvey().getDescription());
